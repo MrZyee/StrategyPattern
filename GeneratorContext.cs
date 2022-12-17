dbf8de171f099dcecd2e01e4fafff4c2ced6ff95
Bartek Koceniak <bartlomiej.koceniak@outlook.com>
2022-12-17 16:38:25 +0100

    Add project files.

---
+++ GeneratorContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyPattern
+{
+    internal class GeneratorContext
+    {
+        private IGenerator _generator;
+        public GeneratorContext(IGenerator generator)
+        {
+            _generator = generator;
+        }
+
+        public void SetGenerator(IGenerator generator)
+        {
+            _generator = generator;
+        }
+
+        public void GenerateFile()
+        {
+            _generator.Generate();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace StrategyPattern 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter n (from 1 to 4): ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            if (n == 1)
+            {
+                var context = new GeneratorContext(new PowerPoint());
+                context.GenerateFile();
+            }
+            else if (n == 2)
+            {
+                var context = new GeneratorContext(new Word());
+                context.GenerateFile();
+            }
+            else if (n == 3)
+            {
+                var context = new GeneratorContext(new Excel());
+                context.GenerateFile();
+            }
+            else if (n == 4)
+            {
+                var context = new GeneratorContext(new Pdf());
+                context.GenerateFile();
+            }
+        }
+    }
+}
